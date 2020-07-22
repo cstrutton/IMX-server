8af39233aeadd425f6e9d2488c3798ca57d69906
Chris Strutton <cstrutto@stackpole.ca>
2020-07-22 12:15:16 -0400

    testing

--- helloworld.py
+++ helloworld.py
@@ -3,5 +3,9 @@ from pylogix import PLC
 with PLC() as comm:
     comm.ProcessorSlot = 3
     comm.IPAddress = '10.4.42.135'
-    result = comm.Read('Batch_Data.Remaining')
+    result = comm.Read(
+        'Program:Production.ProductionData.DailyCounts.DailyTotal')
     print(result)
+
+
+# Program:Production.ProductionData.ShiftParts.Total.DayPartCount
